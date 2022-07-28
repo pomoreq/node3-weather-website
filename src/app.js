564a03fe9ae1f6ffc10c746f28d9c3475801e617
pomoreq <pomor321@wp.pl>
2022-07-28 15:16:34 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utilis/geocode')
 const forecast = require('./utilis/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for Express/static config config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -95,7 +96,7 @@ app.get('*', (req, res) => {
    })
 
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
 
